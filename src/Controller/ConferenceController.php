96eb81d5ea591092a1873323e488f7997cd1f1bc
Galiya <s.galiya.m@gmail.com>
2020-04-16 11:55:25 +0600

    webpack cron

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -15,6 +15,7 @@ use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Messenger\MessageBusInterface;
+use Symfony\Component\Notifier\NotifierInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Twig\Environment;
 
@@ -74,7 +75,9 @@ class ConferenceController extends AbstractController
      * @Route("/conference/{slug}", name="conference")
      */
 
-    public function show(Request $request, Conference $conference, CommentRepository $commentRepository, ConferenceRepository $conferenceRepository, string $photoDir)
+    public function show(Request $request, Conference $conference, CommentRepository $commentRepository,
+                         ConferenceRepository $conferenceRepository,
+                         NotifierInterface $notifier, string $photoDir)
     {
         $comment = new Comment();
         $form = $this->createForm(CommentFormType::class, $comment);
